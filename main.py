1efc37f83d68ba57e9a9e33032ae72b7a7115560
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2025-01-18 12:26:34 -0700

    adding deltatime and fps

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ def main():
   pygame.init()
   print("Starting asteroids!")
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
   while True:
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
           return
     screen.fill("black")
+    tick = clock.tick(60)
+    dt = tick / 1000
     pygame.display.flip()
 
 if __name__ == "__main__":
