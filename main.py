911f28e9f48585fa4d691022fc4abf0a90c0a20d
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2025-01-18 17:26:38 -0700

    adding sprite groups and cleaning up main

--- main.py
+++ main.py
@@ -5,28 +5,34 @@ from player import Player
 
 def main():
   pygame.init()
-  print("Starting asteroids!")
-
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
   dt = 0
 
-  x = SCREEN_WIDTH / 2
-  y = SCREEN_HEIGHT / 2
-  player = Player(x, y)
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  Player.containers = (updatable, drawable)
+
+  player_starting_x = SCREEN_WIDTH / 2
+  player_starting_y = SCREEN_HEIGHT / 2
+  player = Player(player_starting_x, player_starting_y)
 
   while True:
-    tick = clock.tick(60)
-    dt = tick / 1000
-
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
           return
 
     screen.fill("black")
 
-    player.update(dt)
-    player.draw(screen)
+    for sprite in updatable:
+      sprite.update(dt)
+      
+    for sprite in drawable:
+      sprite.draw(screen)
+
+    tick = clock.tick(60)
+    dt = tick / 1000
 
     pygame.display.flip()
 
